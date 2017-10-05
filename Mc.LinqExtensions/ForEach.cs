16e412e43326199eab75dea85fe038d6c8a18902
Manuel <manuelca.eteubeda@gmail.com>
2017-10-05 02:57:02 +0200

    Add ForEach to linq extension project

---
+++ Mc.LinqExtensions/ForEach.cs
@@ -0,0 +1,16 @@
+﻿namespace System.Linq
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static partial class LinqExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> elements, Action<T> action)
+        {
+            foreach (var element in elements)
+            {
+                action(element);
+            }
+        }
+    }
+}
